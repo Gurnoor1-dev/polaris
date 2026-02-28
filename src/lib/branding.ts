eb5229b52837d7ab34583331d6d57f08df8e366f
Gurnoor1-dev <gurnoor123654@gmail.com>
2026-02-28 20:08:43 +0530

    Update branding.ts

--- src/lib/branding.ts
+++ src/lib/branding.ts
@@ -1,1 +1,2 @@
 export const VACOMPANY_URL = "https://community.infiniteflight.com/t/vacompany-powering-the-future-of-virtual-aviation/1151314";
+export const KEVA_URL = "https://community.infiniteflight.com/t/korean-air-virtual-blue-skies-blue-livery-2025-official-thread/852279"
