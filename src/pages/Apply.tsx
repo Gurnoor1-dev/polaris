1bc0c8fdad157665887c3dddb5eb31e713f009de
Gurnoor1-dev <gurnoor123654@gmail.com>
2026-03-01 07:04:49 +0530

    Update Apply.tsx

--- src/pages/Apply.tsx
+++ src/pages/Apply.tsx
@@ -260,7 +260,7 @@ export default function ApplyPage() {
             <div className="mx-auto mb-4">
               <img src={aeroflotLogo} alt="LATOUR Virtual" className="h-12 w-auto object-contain" />
             </div>
-            <CardTitle className="text-2xl">Join LATOUR Virtual</CardTitle>
+            <CardTitle className="text-2xl">Join Korean Air Virtual</CardTitle>
             <CardDescription>
               Complete this form to apply for a pilot position with our virtual airline on Infinite Flight
             </CardDescription>
@@ -382,9 +382,9 @@ export default function ApplyPage() {
                     <Input id="otherVaMembership" value={otherVaMembership} onChange={(e) => setOtherVaMembership(e.target.value)} disabled={isLoading} required />
                   </div>
                   <div className="space-y-2 md:col-span-2">
-                    <Label htmlFor="whyJoinLatour">Why you want to join LATOUR? *</Label>
+                    <Label htmlFor="whyJoinLatour">Why you want to join KEVA? *</Label>
                     <Input id="whyJoinLatour" value={whyJoinLatour} onChange={(e) => setWhyJoinLatour(e.target.value)} disabled={isLoading} required />
-                    <Label htmlFor="hearAboutLatour">Where did you hear about LATOUR? *</Label>
+                    <Label htmlFor="hearAboutLatour">Where did you hear about KEVA? *</Label>
                     <Input id="hearAboutLatour" value={hearAboutLatour} onChange={(e) => setHearAboutLatour(e.target.value)} disabled={isLoading} required />
                   </div>
                 </div>
