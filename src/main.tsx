1df5e2e51de2ce4052858e9e1c19d505e145bb32
Gurnoor1-dev <gurnoor123654@gmail.com>
2026-03-27 20:11:46 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// --- ADD THIS REDIRECT BLOCK START ---
+// This forces the browser to use 'www' so localStorage doesn't get split 
+// between two different domains, which is why your login keeps breaking.
+if (
+  window.location.hostname !== "localhost" && 
+  window.location.hostname === "crewcenterkeva.com"
+) {
+  window.location.replace(
+    "https://www.crewcenterkeva.com" + window.location.pathname + window.location.search
+  );
+}
+// --- ADD THIS REDIRECT BLOCK END ---
+
 createRoot(document.getElementById("root")!).render(<App />);
